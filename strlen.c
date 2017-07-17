9f2f066e4bdaf754f57b8350adc4ce89b429af8d
warnhac <shoaibakhtar6@gmail.com>
2017-07-17 12:23:52 +0530

    Create strlen.c

---
+++ strlen.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+ int user_strlen_with_pointer( char *str)
+ {
+    int str_len =0;
+    while(str)  // it will run utill it found the NULL
+    {
+      ptr++;     // increase the alphabet of the string
+      str_len++; // increase the count of the variable
+    }
+    return str_len;
+ }
+int main()
+{
+
+int result;
+char main_str[1024];
+get(main_str);   
+printf("The length of the string  %d", user_strlen_with_pointer(main_str));
+
+return 0;
+
+}
